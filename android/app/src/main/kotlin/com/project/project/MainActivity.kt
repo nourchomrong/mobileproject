5923cf166e8ad7195b17a7fa7cf717efa4c6b115
nourchomrong <nourchomrong.biu@gmail.com>
2026-04-18 21:17:14 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/project/project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.project.project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/startup_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StartupScreen extends StatelessWidget{
+  const StartupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Startup Screen'),
+      ),
+    );
+  }
+}
